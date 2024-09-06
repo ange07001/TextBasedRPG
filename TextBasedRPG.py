5d84a1e3afdefe23b9e25198ca2428a5a2624f89
ange07001 <ange07001@utb.vaxjo.se>
2024-09-06 09:37:03 +0200

    initial commit

---
+++ TextBasedRPG.py
@@ -0,0 +1,40 @@
+import math
+
+IsMainMenu = True
+error = "\n" + "=" * 10 + "ERROR" + "=" * 10 + "\n"
+
+banner = """
+ _____         _    ______                    _  ____________ _____ 
+|_   _|       | |   | ___ \                  | | | ___ \ ___ \  __ \\
+  | | _____  _| |_  | |_/ / __ _ ___  ___  __| | | |_/ / |_/ / |  \/
+  | |/ _ \ \/ / __| | ___ \/ _` / __|/ _ \/ _` | |    /|  __/| | __ 
+  | |  __/>  <| |_  | |_/ / (_| \__ \  __/ (_| | | |\ \| |   | |_\ \\
+\_/\___/_/\_\\__| \____/ \__,_|___/\___|\__,_| \_| \_\_|    \____/
+
+"""
+
+print(banner)
+
+while IsMainMenu:
+    try:
+        mainMenu = int(input("Main Menu\n1. Play\n2. Exit\n"))
+        if mainMenu == 1:
+            Play = True
+            IsMainMenu = False
+        elif mainMenu == 2:
+            Play = False
+            IsMainMenu = False
+        else:
+            print(error)
+    except ValueError:
+        print(error)
+
+class Player:
+    def __init__(self, health, level, speed, armor, damage):
+        self.health = health
+        self.level = level
+        self.speed = speed
+        self.armor = armor
+        self.damage = damage
+
+player = Player(100, 1, 1, 0, 1)
