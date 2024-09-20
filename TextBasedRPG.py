2040eec44df83bee02a6781aed27a2ce9f1313d7
ange07001 <ange07001@utb.vaxjo.se>
2024-09-20 09:27:45 +0200

    Added new menu and stats page

--- TextBasedRPG.py
+++ TextBasedRPG.py
@@ -1,14 +1,30 @@
 import math
 import json
 import os
+import sys
 
+class Ansi:
+    def Format(text, color):
+        return "\033[0;" + color + "m" + text + "\033[0m"
+    
+    def Print(text, color):
+        print(Ansi.Format(text,color))
+
+    def reset():
+        sys.stdout.write("\033[0m")
+
+play = True
 isMainMenu = True
+isMenu = False
+isStats = False
+
+
+Ansi.reset()
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 filename = 'save_data.json'
 file_path = os.path.join(current_dir, filename)
-
-error = "\n" + "=" * 10 + "ERROR" + "=" * 10 + "\n"
+error = Ansi.Format("\n" + "=" * 10 + "ERROR" + "=" * 10 + "\n","31") 
 
 banner = """
  _____         _    ______                    _  ____________ _____ 
@@ -90,7 +106,9 @@ elif not os.path.exists(file_path):
             "location": "swamps"
         },
         "stats": {
-            "xpGained": 500
+            "itemsLooted": 0,
+            "enemiesDefeated": 0,
+            "timesDied": 0,
         }
     }
 
@@ -98,16 +116,51 @@ elif not os.path.exists(file_path):
 
 print("\n")
 
-while isMainMenu:
-    try:
-        mainMenu = int(input("Main Menu\n[1] Play\n[2] Exit\n"))
-        if mainMenu == 1:
-            play = True
-            isMainMenu = False
-        elif mainMenu == 2:
-            play = False
-            isMainMenu = False
-        else:
+
+
+while play:
+    while isMainMenu:
+        try:
+            mainMenuInput = int(input("Main Menu\n[1] Play\n[2] Exit\n"))
+            if mainMenuInput == 1:
+                isMenu = True
+                isMainMenu = False
+            elif mainMenuInput == 2:
+                isMainMenu = False
+                play = False
+            else:
+                print(error)
+        except ValueError:
             print(error)
-    except ValueError:
-        print(error)
+
+    while isMenu:
+        try:
+            menuInput = int(input("\n[1] Explore \n[2] Inventory \n[3] Stats\n[4] Exit\n"))
+            match menuInput:
+                case 1:
+                    isExplore = True
+                    isMenu = False
+                case 2:
+                    isInventory = True
+                    isMenu = False
+                case 3:
+                    isStats = True 
+                    isMenu = False
+                case 4:
+                    isMainMenu = True
+                    isMenu = False
+                case _:
+                    print(error)
+        except ValueError:
+            print(error)
+
+    while isStats:
+        print(f"\nTotal xp gained: {player.xp} xp")
+        print(f"Items looted: {save_game['stats']['itemsLooted']} items")
+        print(f"Enemies defeated: {save_game['stats']['enemiesDefeated']} enemies")
+        print(f"Times died: {save_game['stats']['timesDied']} deaths")
+        statsInput = int(input("\n[1] Exit\n"))
+        if statsInput == 1:
+            isStats = False
+            isMenu = True
+        
